f5d24fe6e6ef3e9eedd63d0c27e36b6e64356ab6
App Factory <66158498+Appfactory1@users.noreply.github.com>
2020-09-20 01:07:40 +0500

    functional
    haven't check the deleting of images yet

--- src/Actions/productActions.js
+++ src/Actions/productActions.js
@@ -67,47 +67,33 @@ const saveCategory = (product) => async (dispatch) => {
 const saveProduct = (product) => async (dispatch) => {
   try {
     const uuid = uuidv4();
-    console.log(product)
     dispatch({ type: PRODUCT_SAVE_REQUEST, payload: product });
     const uploadTask = storage.ref(`product/${uuid}`).put(product.imageUrl);
-    uploadTask.on(
-      "state_changed",
-      snapshot => {
-        // const progress = Math.round(
-        //   (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-        // );
-        // setProgress(progress);
-      },
-      error => {
-        console.log(error);
-      },
-      () => {
-        storage
-          .ref("product")
-          .child(uuid)
-          .getDownloadURL()
-          .then(url => {
-            product.imageUrl = url;
-            product.imageUuid = uuid;
-          });
+    const image = storage().ref(`product/${uuid}`)
+    image
+    .getDownloadURL()
+    .then(async (url) => {
+      console.log(url);
+      product.imageUrl = url;
+      product.imageUuid = uuid;
+      console.log(product.imageUrl);
+      console.log(product.imageUuid);
+      if (product.id) {
+        const {id, ...remain} = product;
+        const data = await firestore.collection('Products').doc(product.id).set(remain);
+        dispatch({ type: PRODUCT_SAVE_SUCCESS, payload: data });
+      } else {
+        const {id, ...remain} = product;
+        console.log('yippy');
+        const data = await firestore.collection('Products').add(remain);
+        console.log('yippy2');
+        console.log(product)
+        dispatch({ type: PRODUCT_SAVE_SUCCESS, payload: data });
       }
-    );
-    //const {userSignin: { userInfo }} = getState();
-    if (product.id) {
-      const {id, ...remain} = product;
-      const data = await firestore.collection('Products').doc(product.id).set(remain);
-      dispatch({ type: PRODUCT_SAVE_SUCCESS, payload: data });
-    } else {
-      const {id, ...remain} = product;
-      console.log('yippy');
-      const data = await firestore.collection('Products').add(remain);
-      console.log('yippy2');
-      console.log(product)
-      dispatch({ type: PRODUCT_SAVE_SUCCESS, payload: data });
-    }
-  } catch (error) {
-    dispatch({ type: PRODUCT_SAVE_FAIL, payload: error.message });
-  }
+    }) ;
+}catch (error) {
+  dispatch({ type: PRODUCT_SAVE_FAIL, payload: error.message });
+}
 };
 
 const fetchProduct = (productId) => async (dispatch) => {
